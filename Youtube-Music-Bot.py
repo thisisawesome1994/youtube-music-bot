181eb4f7c39069f39969ce24a738346b3b7e699e
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2020-06-28 01:19:33 +0200

    Add files via upload

--- Youtube-Music-Bot.py
+++ Youtube-Music-Bot.py
@@ -5,39 +5,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.chrome.options import Options
+from random import randint
+from time import sleep
 import time
-views = int(input("enter no. of view: "))
-view_time = float(input('enter time for each view: '))
+import os
+import random
 
-# put video link in ''
-for i in range(views):
-    f = open('links.txt', 'r', encoding='utf-8')
-    for line in f:
-        url = line
-    f = open('proxies.txt', 'r', encoding='utf-8')
-    for line in f:
-        proxy = line
-    f = open('username.txt', 'r', encoding='utf-8')
-    for line in f:
-        username = line
-    f = open('password.txt', 'r', encoding='utf-8')
-    for line in f:
-        password = line
-    f = open('useragents.txt', 'r', encoding='utf-8')
-    for line in f:
-        useragent = line
-    opts1 = Options()
-    opts1.add_argument('--user-agent=%s'% useragent)
-    #opts1.add_argument('--mute-audio')
-    opts1.add_argument('--incognito')
-    opts1.add_argument('--proxy-server=%s'% proxy)
-    #opts1.add_argument('--headless')
-    opts1.add_argument('--start-maximized')
-    browser1 = webdriver.Chrome(options=opts1)
-    browser1.execute_script("window.location.replace(arguments[0])", url)
-    time.sleep(10)
-    play_music_1 = browser1.find_element_by_xpath(""" //*[@id="contents"]/yt-button-renderer/a """)
-    play_music_1.click()
-    time.sleep(view_time)
-    browser1.quit()
+proxy = random.choice(open('proxies.txt').readlines())
+useragent = random.choice(open('useragents.txt').readlines())
+url = random.choice(open('links.txt').readlines())
+
+opts1 = Options()
+opts1.add_argument('--user-agent=%s'% useragent)
+#opts1.add_argument('--mute-audio')
+opts1.add_argument('--incognito')
+opts1.add_argument('--proxy-server=%s'% proxy)
+#opts1.add_argument('--headless')
+opts1.add_argument('--start-maximized')
+opts1.add_argument('--disable-gpu')
+
+browser1 = webdriver.Chrome(options=opts1)
+browser1.execute_script("window.location.replace(arguments[0])", url)
+time.sleep(8)
+browser1.find_element_by_xpath(""" //*[@id="contents"]/yt-button-renderer/a """).click()
+time.sleep(randint(180,3200))
+os.startfile("launch.exe")
+browser1.quit()
+
+
 # script by thisisawesome1994
